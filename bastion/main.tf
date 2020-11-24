0a0bdc7830801666e2c5bb609c65c1bbf34eb007
chun613 <chun613@gmail.com>
2020-11-24 19:36:17 +0800

    fix github workflow

--- bastion/main.tf
+++ bastion/main.tf
@@ -12,3 +12,25 @@ terraform {
     }
   }
 }
+
+provider "aws" {
+  region     = "ap-southeast-1"
+  access_key = "AKIAZHO6QESK33KTEJML"
+  secret_key = "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"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
