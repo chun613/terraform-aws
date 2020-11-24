e06fe26036dda9064379c8277a9e7a467eadb8b1
cheng <chun613@gmail.com>
2020-11-24 21:37:13 +0800

    n

--- bastion/main.tf
+++ bastion/main.tf
@@ -18,3 +18,19 @@ provider "aws" {
   access_key = "AKIAZHO6QESK33KTEJML"
   secret_key = "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"
 }
+
+data "aws_ami" "ubuntuu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
